a9b0874686361609a1f8785244f3128a79e49e08
Cary Clark <clarkcb@gmail.com>
2022-09-11 19:33:40 -0700

    Updates/fixes in dart version

--- dart/dartfind/bin/dartfind.sh
+++ dart/dartfind/bin/dartfind.sh
@@ -9,4 +9,5 @@ DARTFIND_PATH=$XFIND_PATH/dart/dartfind
 PACKAGES_PATH=$DARTFIND_PATH/.packages
 DARTFIND_EXE=$DARTFIND_PATH/bin/dartfind.dart
 
-dart --packages="$PACKAGES_PATH" "$DARTFIND_EXE" "$@"
+# dart --packages="$PACKAGES_PATH" "$DARTFIND_EXE" "$@"
+dart "$DARTFIND_EXE" "$@"
