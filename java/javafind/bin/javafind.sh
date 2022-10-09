2c3e8f445fbb086835681bb6cdf09060dff70433
Cary Clark <clarkcb@gmail.com>
2022-10-08 18:31:56 -0700

    Add version to javafind.sh script in java version

--- java/javafind/bin/javafind.sh
+++ java/javafind/bin/javafind.sh
@@ -6,6 +6,9 @@ then
 fi
 
 JAVAFIND_PATH=$XFIND_PATH/java/javafind
-JAVAFIND_JAR=$(find $JAVAFIND_PATH/target -name "javafind*.jar" | head -n 1)
+JAVAFIND_VERSION="0.1.0-SNAPSHOT"
+# JAVAFIND_JAR=$(find $JAVAFIND_PATH/target -name "javafind*.jar" | head -n 1)
+JAVAFIND_JAR="$JAVAFIND_PATH/target/javafind-$JAVAFIND_VERSION.jar"
 
+# echo "java -cp $JAVAFIND_JAR javafind.FindMain $@"
 java -cp "$JAVAFIND_JAR" javafind.FindMain "$@"
