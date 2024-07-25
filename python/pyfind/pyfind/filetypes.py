dc6270afc0016bbc0bf962d1ac45e4f7544f153a
Cary Clark <clarkcb@gmail.com>
2024-07-25 11:54:55 -0700

    Get filetype from sqlite in python version

--- python/pyfind/pyfind/filetypes.py
+++ python/pyfind/pyfind/filetypes.py
@@ -8,39 +8,43 @@
 #
 ###############################################################################
 """
-import importlib.resources
-import json
-from enum import StrEnum
+from enum import Enum
 from pathlib import Path
+import sqlite3
 
+from .config import XFINDDB
 from .fileutil import FileUtil
 
 
-class FileType(StrEnum):
+class FileType(Enum):
     """FileType enum"""
-    UNKNOWN = 'unknown'
-    ARCHIVE = 'archive'
-    AUDIO = 'audio'
-    BINARY = 'binary'
-    CODE = 'code'
-    FONT = 'font'
-    IMAGE = 'image'
-    TEXT = 'text'
-    VIDEO = 'video'
-    XML = 'xml'
+    UNKNOWN = 1
+    ARCHIVE = 2
+    AUDIO = 3
+    BINARY = 4
+    CODE = 5
+    FONT = 6
+    IMAGE = 7
+    TEXT = 8
+    VIDEO = 9
+    XML = 10
+
+    @classmethod
+    def from_name(cls, name: str):
+        """Return FileType for given name if found else return UNKNOWN"""
+        uname = name.upper()
+        fts = [_ft for _ft in FileType if _ft.name == uname]
+        if fts:
+            return fts[0]
+        return FileType.UNKNOWN
 
     def __lt__(self, other):
         if self.__class__ is other.__class__:
             return self.value < other.value
         return NotImplemented
 
-    @classmethod
-    def from_name(cls, name):
-        """Return FileType for given name if found else return UNKNOWN"""
-        try:
-            return FileType[name.strip().upper()]
-        except KeyError:
-            return FileType.UNKNOWN
+    def __str__(self):
+        return self.name.lower()
 
 
 class FileTypes:
@@ -51,112 +55,92 @@ class FileTypes:
     ZIPFILE_EXTENSIONS = frozenset(['zip', 'jar', 'war', 'ear', 'obr', 'apk', 'xpi', 'xap', 'xar', 'epub'])
     TARFILE_EXTENSIONS = frozenset(['tar', 'tgz', 'bz2', 'gz', 'xz', 'Z', 'lz', 'lzma'])
 
-    __slots__ = ['__file_type_exts', '__file_type_names']
+    __slots__ = ['__db_connection', '__db_cursor', '__file_types', '__ext_file_type_cache']
 
     def __init__(self):
-        self.__file_type_exts = {}
-        self.__file_type_names = {}
-        self.__populate_file_types_from_json()
+        self.__db_connection = None
+        self.__db_cursor = None
+        self.__file_types = list(FileType)
+        self.__ext_file_type_cache = {}
+
+    def __query_one(self, query: str, params=None):
+        """Return one result from query"""
+        if not self.__db_cursor:
+            self.__db_cursor = self.__get_cursor()
+        if not params:
+            params = []
+        return self.__db_cursor.execute(query, params).fetchone()
+
+    def get_file_type_for_name(self, name: str) -> FileType:
+        """Return FileType for given file name"""
+        query = 'SELECT file_type_id FROM file_name WHERE name = ?'
+        params = (name,)
+        res = self.__query_one(query, params)
+        if res:
+            return self.__file_types[res[0] - 1]
+        return FileType.UNKNOWN
+
+    def get_file_type_for_extension(self, ext: str) -> FileType:
+        """Return FileType for given file extension"""
+        if ext in self.__ext_file_type_cache:
+            return self.__ext_file_type_cache[ext]
+        query = 'SELECT file_type_id FROM file_extension WHERE extension = ?'
+        params = (ext,)
+        res = self.__query_one(query, params)
+        if res:
+            return self.__file_types[res[0] - 1]
+        return FileType.UNKNOWN
 
     def get_file_type_for_path(self, file_path: Path) -> FileType:
         """Return file type for file_path"""
-        # more specific first
-        if self.is_code_file_path(file_path):
-            return FileType.CODE
-        if self.is_archive_file_path(file_path):
-            return FileType.ARCHIVE
-        if self.is_audio_file_path(file_path):
-            return FileType.AUDIO
-        if self.is_font_file_path(file_path):
-            return FileType.FONT
-        if self.is_image_file_path(file_path):
-            return FileType.IMAGE
-        if self.is_video_file_path(file_path):
-            return FileType.VIDEO
-        # more general last
-        if self.is_xml_file_path(file_path):
-            return FileType.XML
-        if self.is_text_file_path(file_path):
-            return FileType.TEXT
-        if self.is_binary_file_path(file_path):
-            return FileType.BINARY
-        return FileType.UNKNOWN
+        file_type_for_file_name = self.get_file_type_for_name(file_path.name)
+        if file_type_for_file_name != FileType.UNKNOWN:
+            return file_type_for_file_name
+        return self.get_file_type_for_extension(FileUtil.get_path_extension(file_path))
 
     def is_archive_file_path(self, p: Path) -> bool:
         """Return true if file is of a (known) archive file type"""
-        return p.name in self.__file_type_names[FileType.ARCHIVE] or \
-            FileUtil.get_path_extension(p) in self.__file_type_exts[FileType.ARCHIVE]
+        return self.get_file_type_for_path(p) == FileType.ARCHIVE
 
     def is_audio_file_path(self, p: Path) -> bool:
         """Return true if file is of a (known) audio file type"""
-        return p.name in self.__file_type_names[FileType.AUDIO] or \
-            FileUtil.get_path_extension(p) in self.__file_type_exts[FileType.AUDIO]
+        return self.get_file_type_for_path(p) == FileType.AUDIO
 
     def is_binary_file_path(self, p: Path) -> bool:
         """Return true if file is of a (known) findable binary file type"""
-        return p.name in self.__file_type_names[FileType.BINARY] or \
-            FileUtil.get_path_extension(p) in self.__file_type_exts[FileType.BINARY]
+        return self.get_file_type_for_path(p) == FileType.BINARY
 
     def is_code_file_path(self, p: Path) -> bool:
         """Return true if file is of a (known) code file type"""
-        return p.name in self.__file_type_names[FileType.CODE] or \
-            FileUtil.get_path_extension(p) in self.__file_type_exts[FileType.CODE]
+        return self.get_file_type_for_path(p) == FileType.CODE
 
     def is_font_file_path(self, p: Path) -> bool:
         """Return true if file is of a (known) font file type"""
-        return p.name in self.__file_type_names[FileType.FONT] or \
-            FileUtil.get_path_extension(p) in self.__file_type_exts[FileType.FONT]
+        return self.get_file_type_for_path(p) == FileType.FONT
 
     def is_image_file_path(self, p: Path) -> bool:
         """Return true if file is of a (known) image file type"""
-        return p.name in self.__file_type_names[FileType.IMAGE] or \
-            FileUtil.get_path_extension(p) in self.__file_type_exts[FileType.IMAGE]
+        return self.get_file_type_for_path(p) == FileType.IMAGE
 
     def is_text_file_path(self, p: Path) -> bool:
         """Return true if file is of a (known) text file type"""
-        return p.name in self.__file_type_names[FileType.TEXT] or \
-            FileUtil.get_path_extension(p) in self.__file_type_exts[FileType.TEXT]
+        file_type = self.get_file_type_for_path(p)
+        return file_type == FileType.TEXT or file_type == FileType.CODE or file_type == FileType.XML
 
     def is_video_file_path(self, p: Path) -> bool:
         """Return true if file is of a (known) video file type"""
-        return p.name in self.__file_type_names[FileType.VIDEO] or \
-            FileUtil.get_path_extension(p) in self.__file_type_exts[FileType.VIDEO]
+        return self.get_file_type_for_path(p) == FileType.VIDEO
 
     def is_xml_file_path(self, p: Path) -> bool:
         """Return true if file is of a (known) xml file type"""
-        return str(p) in self.__file_type_names[FileType.XML] or \
-            FileUtil.get_path_extension(p) in self.__file_type_exts[FileType.XML]
+        return self.get_file_type_for_path(p) == FileType.XML
 
     def is_unknown_file_path(self, p: Path) -> bool:
         """Return true if file is of an unknown file type"""
         return self.get_file_type_for_path(p) == FileType.UNKNOWN
 
-    def __populate_file_types_from_json(self):
-        data = importlib.resources.files('pyfind').joinpath('data')
-        file_types_json = data.joinpath('filetypes.json').read_text()
-        file_types_dict = json.loads(file_types_json)
-        for file_type_obj in file_types_dict['filetypes']:
-            typename = file_type_obj['type']
-            self.__file_type_exts[typename] = set(file_type_obj['extensions'])
-            if 'names' in file_type_obj:
-                self.__file_type_names[typename] = set(file_type_obj['names'])
-            else:
-                self.__file_type_names[typename] = set([])
-        self.__file_type_exts['text'].update(self.__file_type_exts['code'],
-                                             self.__file_type_exts['xml'])
-        self.__file_type_names['text'].update(self.__file_type_names['code'],
-                                              self.__file_type_names['xml'])
-        self.__file_type_exts['findable'] = \
-            self.__file_type_exts['binary'].union(self.__file_type_exts['archive'],
-                                                  self.__file_type_exts['audio'],
-                                                  self.__file_type_exts['font'],
-                                                  self.__file_type_exts['image'],
-                                                  self.__file_type_exts['text'],
-                                                  self.__file_type_exts['video'])
-        self.__file_type_names['findable'] = \
-            self.__file_type_names['binary'].union(self.__file_type_names['archive'],
-                                                   self.__file_type_names['audio'],
-                                                   self.__file_type_names['font'],
-                                                   self.__file_type_names['image'],
-                                                   self.__file_type_names['text'],
-                                                   self.__file_type_names['video'])
+    def __get_cursor(self):
+        """Return a connection to the database"""
+        if not self.__db_connection:
+            self.__db_connection = sqlite3.connect(f'file:{XFINDDB}?mode=ro', uri=True)
+        return self.__db_connection.cursor()
