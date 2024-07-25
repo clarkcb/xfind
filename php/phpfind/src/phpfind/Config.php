917d3d452c756d6dd634a504fe18b1a720c18c82
Cary Clark <clarkcb@gmail.com>
2024-07-25 14:38:40 -0700

    Get filetype from sqlite in php version

--- php/phpfind/src/phpfind/Config.php
+++ php/phpfind/src/phpfind/Config.php
@@ -20,12 +20,14 @@ $resources_path = FileUtil::join_paths(__DIR__, '..', '..', 'resources');
 $file_types_path = FileUtil::join_paths($resources_path, 'filetypes.json');
 $find_options_path = FileUtil::join_paths($resources_path, 'findoptions.json');
 $default_find_settings_path = FileUtil::join_paths($home, '.config', 'xfind', 'settings.json');
+$xfind_db_path = FileUtil::join_paths($shared_path, 'xfind.db');
 
 define('Z_XFIND_PATH', $xfind_path);
 define('Z_XFIND_SHARED_PATH', $xfind_shared_path);
 define('Z_FILE_TYPES_PATH', $file_types_path);
 define('Z_FIND_OPTIONS_PATH', $find_options_path);
 define('Z_DEFAULT_FIND_SETTINGS_PATH', $default_find_settings_path);
+define('Z_XFIND_DB', $xfind_db_path);
 
 class Config
 {
@@ -34,4 +36,5 @@ class Config
     const string FILE_TYPES_PATH = Z_FILE_TYPES_PATH;
     const string FIND_OPTIONS_PATH = Z_FIND_OPTIONS_PATH;
     const string DEFAULT_FIND_SETTINGS_PATH = Z_DEFAULT_FIND_SETTINGS_PATH;
+    const string XFIND_DB = Z_XFIND_DB;
 }
