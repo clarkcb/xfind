57284a503675d28a7bbfe53f85413b54c4056bab
Cary Clark <clarkcb@gmail.com>
2024-07-26 12:11:40 -0700

    Get filetype from sqlite in swift version

--- swift/swiftfind/Package.swift
+++ swift/swiftfind/Package.swift
@@ -11,15 +11,16 @@ let package = Package(
         .executable(name: "swiftfindApp", targets: ["swiftfindApp"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // Despite the warning about being deprecated, name must be included here
+        // because there's a discrepancy between its package name and target name
+        .package(name: "SQLite", url: "https://github.com/stephencelis/SQLite.swift.git", .upToNextMajor(from: "0.15.3")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "swiftfind",
-            dependencies: []),
+            dependencies: ["SQLite"]),
         .executableTarget(
             name: "swiftfindApp",
             dependencies: ["swiftfind"]),
