47f301b9a7010c262d7baff6c3e796d71878a910
Cary Clark <clarkcb@gmail.com>
2024-07-26 16:09:54 -0700

    Get filetype from sqlite in C version

--- c/cfind/include/config.h
+++ c/cfind/include/config.h
@@ -4,6 +4,7 @@
 #define MAX_HOMEPATH_LENGTH 100
 #define FILE_TYPES_REL_PATH "shared/filetypes.json"
 #define FIND_OPTIONS_REL_PATH "shared/findoptions.json"
+#define XFIND_DB_REL_PATH "shared/xfind.db"
 
 void get_home_path(char *dest);
 
@@ -12,5 +13,7 @@ void get_xfind_path(char *dest);
 void get_file_types_path(char *dest);
 
 void get_find_options_path(char *dest);
+
+void get_xfind_db_path(char *dest);
 
 #endif
