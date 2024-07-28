970e82056fd9a50301f428ab104ddb748557b141
Cary Clark <clarkcb@gmail.com>
2024-07-27 18:08:26 -0700

    Get filetype from sqlite in haskell version

--- haskell/hsfind/src/HsFind/FileTypes.hs
+++ haskell/hsfind/src/HsFind/FileTypes.hs
@@ -1,28 +1,31 @@
-{-# LANGUAGE DeriveGeneric, NoMonomorphismRestriction, OverloadedStrings #-}
+{-# LANGUAGE NoMonomorphismRestriction, OverloadedStrings #-}
 module HsFind.FileTypes
   ( FileType(..)
-  , JsonFileType(..)
-  , getFileType
-  , getFileTypes
   , getFileTypeForName
-  , getFileTypeFromJsonFileTypes
-  , getFileTypesFromJsonFileTypes
   , getFileTypeName
-  , getJsonFileTypes
-  , fileTypeFromJsonFileTypes
+  , getDbFileType
+  , getDbFileTypes
+  , isArchiveFilePath
+  , isAudioFilePath
+  , isBinaryFilePath
+  , isCodeFilePath
+  , isFontFilePath
+  , isImageFilePath
+  , isTextFilePath
+  , isVideoFilePath
+  , isXmlFilePath
+  , isUnknownFilePath
   ) where
 
-import qualified Data.ByteString.Lazy.Char8 as BC
 import Data.Char (toLower)
-import Data.Text(pack, unpack, replace)
-
-import GHC.Generics
-import Data.Aeson
-
+import Data.List (intercalate, nub, partition)
+import qualified Data.Text as T
 import System.FilePath (takeFileName)
 
-import HsFind.FileUtil (getExtension, normalizeExtension, getFileString)
-import HsFind.Paths_hsfind (getDataFileName)
+import Database.SQLite.Simple
+
+import HsFind.FileUtil (getExtension)
+import Data.Maybe (mapMaybe)
 
 data FileType = Unknown
               | Archive
@@ -36,11 +39,8 @@ data FileType = Unknown
               | Xml
   deriving (Show, Eq, Ord)
 
--- searchableFileTypes :: [FileType]
--- searchableFileTypes = [Archive, Binary, Code, Image, Text, Xml]
-
--- isSearchableFileType :: FileType -> Bool
--- isSearchableFileType t = t `elem` searchableFileTypes
+fileTypes :: [FileType]
+fileTypes = [Unknown, Archive, Audio, Binary, Code, Font, Image, Text, Video, Xml]
 
 getFileTypeForName :: String -> FileType
 getFileTypeForName typeName =
@@ -71,99 +71,141 @@ getFileTypeName ft =
     Xml -> "xml"
     _ -> "unknown"
 
-data JsonFileType = JsonFileType
-    { fileType :: String
-    , extensions :: [String]
-    , names :: [String]
-    } deriving (Show, Eq, Generic)
+batchSize :: Int
+batchSize = 500
 
-instance FromJSON JsonFileType where
-  parseJSON = genericParseJSON defaultOptions {
-                fieldLabelModifier = unpack . replace "fileType" "type" . pack
-              }
+getDbFileTypesForQueryAndElems :: Connection -> [String] -> [String] -> IO [(String, FileType)]
+getDbFileTypesForQueryAndElems conn queryElems [] = return []
+getDbFileTypesForQueryAndElems conn queryElems elems = do
+  let uniqElems = nub elems
+  getFileTypesForElems uniqElems []
+  where getFileTypesForElems :: [String] -> [(String, FileType)] -> IO [(String, FileType)]
+        getFileTypesForElems elems elemsAndTypes =
+          case elems of
+            [] -> return elemsAndTypes
+            _ -> do
+              let elemBatch = take batchSize elems
+              let placeholders = intercalate ", " $ replicate (length elemBatch) "?" :: String
+              let selectStr = concatMap (\q -> if q == "?" then placeholders else q) queryElems :: String
+              let selectText = T.pack selectStr
+              let selectQuery = Query {fromQuery=selectText}
+              rows <- query conn selectQuery elemBatch :: IO [(String, Int)]
+              let dbElemBatchTypes = map (\(e, i) -> (e, fileTypes !! (i - 1))) rows
+              let elemBatchAndTypes = getDbFileTypesForElems elemBatch dbElemBatchTypes []
+              getFileTypesForElems (drop batchSize elems) $ elemsAndTypes ++ elemBatchAndTypes
+        getDbFileTypesForElems :: [String] -> [(String, FileType)] -> [(String, FileType)] -> [(String, FileType)]
+        getDbFileTypesForElems elems dbFileElemTypes elemsAndTypes =
+          case elems of
+            [] -> elemsAndTypes
+            (e:elems') ->
+              case lookup e dbFileElemTypes of
+                Just ft -> getDbFileTypesForElems elems' dbFileElemTypes $ elemsAndTypes ++ [(e, ft)]
+                Nothing -> getDbFileTypesForElems elems' dbFileElemTypes $ elemsAndTypes ++ [(e, Unknown)]
 
-newtype JsonFileTypes
-  = JsonFileTypes {filetypes :: [JsonFileType]}
-  deriving (Show, Eq, Generic)
+getDbFileTypesForFileNames :: Connection -> [FilePath] -> IO [(String, FileType)]
+getDbFileTypesForFileNames conn [] = return []
+getDbFileTypesForFileNames conn filePaths = do
+  let queryElems = ["SELECT name, file_type_id FROM file_name WHERE name IN (", "?", ")"]
+  let fileNames = map takeFileName filePaths
+  getDbFileTypesForQueryAndElems conn queryElems fileNames
 
-instance FromJSON JsonFileTypes
+getDbFileTypeForFileName :: Connection -> FilePath -> IO FileType
+getDbFileTypeForFileName conn fileName = do
+  row <- query conn "SELECT file_type_id FROM file_name WHERE name = ?" (Only (fileName :: String)) :: IO [Only Int]
+  case row of
+    [Only i] -> return $ fileTypes !! (i - 1)
+    _ -> return Unknown
 
-fileTypesJsonFile :: FilePath
-fileTypesJsonFile = "filetypes.json"
+getDbFileTypesForExtensions :: Connection -> [String] -> IO [(String, FileType)]
+getDbFileTypesForExtensions conn [] = return []
+getDbFileTypesForExtensions conn fileExts = do
+  let queryElems = ["SELECT extension, file_type_id FROM file_extension WHERE extension IN (", "?", ")"]
+  getDbFileTypesForQueryAndElems conn queryElems fileExts
 
-getJsonFileTypes :: IO [JsonFileType]
-getJsonFileTypes = do
-  fileTypesJsonPath <- getDataFileName fileTypesJsonFile
-  fileTypesJsonString <- getFileString fileTypesJsonPath
-  case fileTypesJsonString of
-    (Left _) -> return []
-    (Right jsonString) ->
-      case (eitherDecode (BC.pack jsonString) :: Either String JsonFileTypes) of
-        (Left _) -> return []
-        (Right jsonFileTypes) -> return (map normalizeType (filetypes jsonFileTypes))
-  where normalizeType :: JsonFileType -> JsonFileType
-        normalizeType ft = JsonFileType { fileType = fileType ft,
-                                          extensions = map normalizeExtension (extensions ft) ,
-                                          names = names ft }
+getDbFileTypeForExtension :: Connection -> String -> IO FileType
+getDbFileTypeForExtension conn fileExt = do
+  row <- query conn "SELECT file_type_id FROM file_extension WHERE extension = ?" (Only (fileExt :: String)) :: IO [Only Int]
+  case row of
+    [Only i] -> return $ fileTypes !! (i - 1)
+    _ -> return Unknown
 
-getFileType :: FilePath -> IO FileType
-getFileType f = do
-  fileTypes <- getFileTypes [f]
-  case fileTypes of
-    [] -> return Unknown
-    _  -> return $ head fileTypes
+getDbFileType :: Connection -> FilePath -> IO FileType
+getDbFileType conn f = do
+  ft <- getDbFileTypeForFileName conn (takeFileName f)
+  case ft of
+    Unknown -> case getExtension f of
+                  Just x -> getDbFileTypeForExtension conn x
+                  Nothing -> return Unknown
+    _  -> return ft
 
-getFileTypes :: [FilePath] -> IO [FileType]
-getFileTypes files = do
-  jsonFileTypes <- getJsonFileTypes
-  return $ map (fileTypeFromJsonFileTypes jsonFileTypes) files
+-- getDbFileTypes :: Connection -> [FilePath] -> IO [FileType]
+-- getDbFileTypes conn filePaths = do
+--   mapM (getDbFileType conn) filePaths
 
-getFileTypeFromJsonFileTypes :: [JsonFileType] -> FilePath -> FileType
-getFileTypeFromJsonFileTypes = fileTypeFromJsonFileTypes
+getDbFileTypes :: Connection -> [FilePath] -> IO [FileType]
+getDbFileTypes conn filePaths = do
+  dbFileTypesForFileNames <- getDbFileTypesForFileNames conn $ map takeFileName filePaths
+  let (knownTypesForFileNames, unknownTypesForFileNames) = partition (\(name, ft) -> ft /= Unknown) dbFileTypesForFileNames
+  let unknownFileNames = map fst unknownTypesForFileNames
+  dbFileTypesForExts <- getDbFileTypesForExtensions conn $ mapMaybe getExtension unknownFileNames
+  return $ getFilePathsWithFileTypes filePaths knownTypesForFileNames dbFileTypesForExts []
+  where getFilePathsWithFileTypes :: [FilePath] -> [(FilePath, FileType)] -> [(FilePath, FileType)] -> [(FilePath, FileType)] -> [FileType]
+        getFilePathsWithFileTypes paths knownTypesForFileNames dbFileTypesForExts pathsAndTypes =
+          case paths of
+            [] -> map snd pathsAndTypes
+            (path:paths') ->
+              case lookup (takeFileName path) knownTypesForFileNames of
+                Just ft -> getFilePathsWithFileTypes paths' knownTypesForFileNames dbFileTypesForExts $ pathsAndTypes ++ [(path, ft)]
+                Nothing ->
+                  case getExtension path of
+                    Just ext ->
+                      case lookup ext dbFileTypesForExts of
+                        Just ft -> getFilePathsWithFileTypes paths' knownTypesForFileNames dbFileTypesForExts $ pathsAndTypes ++ [(path, ft)]
+                        Nothing -> getFilePathsWithFileTypes paths' knownTypesForFileNames dbFileTypesForExts $ pathsAndTypes ++ [(path, Unknown)]
+                    Nothing -> getFilePathsWithFileTypes paths' knownTypesForFileNames dbFileTypesForExts $ pathsAndTypes ++ [(path, Unknown)]
 
-getFileTypesFromJsonFileTypes :: [JsonFileType] -> [FilePath] -> [FileType]
-getFileTypesFromJsonFileTypes jsonFileTypes =
-  map (fileTypeFromJsonFileTypes jsonFileTypes)
+hasFilePathFileType :: Connection -> FilePath -> FileType -> IO Bool
+hasFilePathFileType conn fp ft = do
+  dbFileType <- getDbFileType conn fp
+  return $ dbFileType == ft
 
-fileTypeFromJsonFileTypes :: [JsonFileType] -> FilePath -> FileType
-fileTypeFromJsonFileTypes jsonFileTypes fp =
-  case matchingTypeForNameJson jsonFileTypes fileName of
-    Unknown -> case getExtension fileName of
-                  Just x -> matchingTypeForExtensionJson jsonFileTypes x
-                  Nothing -> Unknown
-    ft -> ft
-  where fileName = takeFileName fp
+isUnknownFilePath :: Connection -> FilePath -> IO Bool
+isUnknownFilePath conn fp = do
+  hasFilePathFileType conn fp Unknown
 
-matchingTypeForNameJson :: [JsonFileType] -> String -> FileType
-matchingTypeForNameJson jsonFileTypes n =
-  case filter (\f -> n `elem` names f) jsonFileTypes of
-    [] -> Unknown
-    fts -> case fileTypeName fts of
-           "archive" -> Archive
-           "audio" -> Audio
-           "code" -> Code
-           "font" -> Font
-           "image" -> Image
-           "video" -> Video
-           "xml" -> Xml
-           "binary" -> Binary
-           tname | tname `elem` ["code", "text", "xml"] -> Text
-           _ -> Unknown
-  where fileTypeName = fileType . head
+isArchiveFilePath :: Connection -> FilePath -> IO Bool
+isArchiveFilePath conn fp = do
+  hasFilePathFileType conn fp Archive
 
-matchingTypeForExtensionJson :: [JsonFileType] -> String -> FileType
-matchingTypeForExtensionJson jsonFileTypes x =
-  case filter (\f -> x `elem` extensions f) jsonFileTypes of
-    [] -> Unknown
-    fts -> case fileTypeName fts of
-           "archive" -> Archive
-           "audio" -> Audio
-           "code" -> Code
-           "font" -> Font
-           "image" -> Image
-           "video" -> Video
-           "xml" -> Xml
-           "binary" -> Binary
-           tname | tname `elem` ["code", "text", "xml"] -> Text
-           _ -> Unknown
-  where fileTypeName = fileType . head
+isAudioFilePath :: Connection -> FilePath -> IO Bool
+isAudioFilePath conn fp = do
+  hasFilePathFileType conn fp Audio
+
+isBinaryFilePath :: Connection -> FilePath -> IO Bool
+isBinaryFilePath conn fp = do
+  hasFilePathFileType conn fp Binary
+
+isCodeFilePath :: Connection -> FilePath -> IO Bool
+isCodeFilePath conn fp = do
+  hasFilePathFileType conn fp Code
+
+isFontFilePath :: Connection -> FilePath -> IO Bool
+isFontFilePath conn fp = do
+  hasFilePathFileType conn fp Font
+
+isImageFilePath :: Connection -> FilePath -> IO Bool
+isImageFilePath conn fp = do
+  hasFilePathFileType conn fp Image
+
+isTextFilePath :: Connection -> FilePath -> IO Bool
+isTextFilePath conn fp = do
+  dbFileType <- getDbFileType conn fp
+  return $ dbFileType `elem` [Text, Code, Xml]
+
+isVideoFilePath :: Connection -> FilePath -> IO Bool
+isVideoFilePath conn fp = do
+  hasFilePathFileType conn fp Video
+
+isXmlFilePath :: Connection -> FilePath -> IO Bool
+isXmlFilePath conn fp = do
+  hasFilePathFileType conn fp Xml
