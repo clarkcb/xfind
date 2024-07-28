76a1509aa7a9070065451fa314d185c71fb4037f
Cary Clark <clarkcb@gmail.com>
2024-07-27 18:08:26 -0700

    Get filetype from sqlite in haskell version

--- haskell/hsfind/src/HsFind/Config.hs
+++ haskell/hsfind/src/HsFind/Config.hs
@@ -1,6 +1,7 @@
 module HsFind.Config
   (
       getXfindPath
+    , getXfindDbPath
     , getDataPath
     , getHome
   ) where
@@ -26,6 +27,11 @@ getXfindPath = do
   case maybeXfindPath of
     Just xfindPath -> return xfindPath
     Nothing -> return $ home ++ "/src/xfind"
+
+getXfindDbPath :: IO FilePath
+getXfindDbPath = do
+  xfindPath <- getXfindPath
+  return $ xfindPath ++ "/shared/xfind.db"
 
 getDataPath :: IO FilePath
 getDataPath = do
