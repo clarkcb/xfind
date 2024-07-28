bf3c23dda1a19cb8a98b61ccfb0cc8f12a987d06
Cary Clark <clarkcb@gmail.com>
2024-07-27 18:08:26 -0700

    Get filetype from sqlite in haskell version

--- haskell/hsfind/src/HsFind/Finder.hs
+++ haskell/hsfind/src/HsFind/Finder.hs
@@ -19,7 +19,10 @@ import System.FilePath (takeDirectory, takeFileName)
 import Text.Regex.PCRE ( (=~) )
 import Data.Time (UTCTime)
 
-import HsFind.FileTypes (FileType(..), JsonFileType, getJsonFileTypes, getFileTypesFromJsonFileTypes)
+import Database.SQLite.Simple
+
+import HsFind.Config (getXfindDbPath)
+import HsFind.FileTypes (FileType(..), getDbFileTypes)
 import HsFind.FileUtil
     (expandPath, filterDirectories, filterFiles, filterOutSymlinks, hasExtension, isHiddenFilePath,
     getNonDotDirectoryContents, getFileSizes, getModificationTimes, partitionDirsAndFiles,
@@ -184,8 +187,8 @@ matchesFileResultTests :: [FileResult -> Bool] -> FileResult -> Bool
 matchesFileResultTests [] _ = True
 matchesFileResultTests tests fr = all ($fr) tests
 
-filterToFileResult :: FindSettings -> [JsonFileType] -> (FilePath,FileType) -> Maybe FileResult
-filterToFileResult settings jsonFileTypes ft =
+filterToFileResult :: FindSettings -> (FilePath,FileType) -> Maybe FileResult
+filterToFileResult settings ft =
   if (null inTypes || snd ft `elem` inTypes) && (null outTypes || notElem (snd ft) outTypes)
   then Just $ uncurry newFileResult ft
   else Nothing
@@ -229,13 +232,14 @@ getRecursiveFilePaths settings dir = do
 
 getFileResults :: FindSettings -> IO [FileResult]
 getFileResults settings = do
+  xfindDbPath <- getXfindDbPath
+  conn <- open xfindDbPath
   pathDirs <- filterDirectories (paths settings)
   pathFiles <- filterFiles (paths settings)
   pathLists <- forM pathDirs $ \path ->
     getRecursiveFilePaths settings path
   let allPaths = concat pathLists ++ pathFiles
-  jsonFileTypes <- getJsonFileTypes
-  let allFileTypes = getFileTypesFromJsonFileTypes jsonFileTypes allPaths
+  allFileTypes <- getDbFileTypes conn allPaths
   let allPathsAndTypes = zip allPaths allFileTypes
   let fileTypesFilter = matchesFileTypeTests $ getFileTypeTests settings
   let filteredPathsAndTypes = filter (\(_, ft) -> fileTypesFilter ft) allPathsAndTypes
