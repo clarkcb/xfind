73b68d1a5fb020c5f211aa7b500b22a7d6186953
Cary Clark <clarkcb@gmail.com>
2024-08-03 18:54:44 -0700

    Use path.join for config in dart version

--- dart/dartfind/lib/src/config.dart
+++ dart/dartfind/lib/src/config.dart
@@ -1,9 +1,10 @@
 import 'dart:io' show Platform;
+import 'package:path/path.dart' as path;
 
 final String xFindPath = Platform.environment.containsKey('XFIND_PATH')
     ? Platform.environment['XFIND_PATH']!
-    : '${Platform.environment['HOME']!}/src/xfind';
-final String sharedPath = '$xFindPath/shared';
-final String fileTypesPath = '$sharedPath/filetypes.json';
-final String findOptionsPath = '$sharedPath/findoptions.json';
-final String xfindDb = '$sharedPath/xfind.db';
+    : path.join(Platform.environment['HOME']!, 'src', 'xfind');
+final String sharedPath = path.join(xFindPath, 'shared');
+final String fileTypesPath = path.join(sharedPath, 'filetypes.json');
+final String findOptionsPath = path.join(sharedPath, 'findoptions.json');
+final String xfindDb = path.join(sharedPath, 'xfind.db');
