31d935635e0acdd881d930cd56ba12bc439e9abc
Cary Clark <clarkcb@gmail.com>
2024-08-31 19:28:41 -0700

    Use ext, name caches in objc version

--- objc/objcfind/Sources/objcfind/FileTypes.m
+++ objc/objcfind/Sources/objcfind/FileTypes.m
@@ -17,7 +17,9 @@
 @property NSString *xml;
 
 @property sqlite3 *db;
-@property NSMutableDictionary *extTypeCache;
+@property NSMutableDictionary<NSString*,NSNumber*> *extTypeCache;
+@property NSMutableDictionary<NSString*,NSNumber*> *nameTypeCache;
+@property BOOL nameTypeCacheLoaded;
 
 @end
 
@@ -36,14 +38,16 @@
         self.unknown = [NSString stringWithUTF8String:T_UNKNOWN];
         self.video = [NSString stringWithUTF8String:T_VIDEO];
         self.xml = [NSString stringWithUTF8String:T_XML];
-        
+
         NSString *xfindDbPath = getXfindDbPath();
         sqlite3 *db;
         int rc = sqlite3_open_v2([xfindDbPath UTF8String], &db, SQLITE_OPEN_READONLY, nil);
         if (rc == SQLITE_OK && db != nil) {
             self.db = db;
         }
-        self.extTypeCache = [[NSMutableDictionary alloc] initWithCapacity:10];
+        self.extTypeCache = [[NSMutableDictionary alloc] initWithCapacity:16];
+        self.nameTypeCache = [[NSMutableDictionary alloc] initWithCapacity:32];
+        self.nameTypeCacheLoaded = false;
     }
     return self;
 }
@@ -111,13 +115,41 @@
     return [NSString stringWithUTF8String:T_UNKNOWN];
 }
 
-- (FileType) getFileTypeForQuery:(NSString*)query andElem:(NSString*)elem {
+- (NSDictionary<NSString*,NSNumber*>*) getFileTypesForQuery:(NSString*)query andParams:(NSArray<NSString*>*)params {
+    NSMutableDictionary *results = [[NSMutableDictionary alloc] init];
+    sqlite3_stmt *stmt;
+    sqlite3_prepare_v2(self.db, [query UTF8String], -1, &stmt, NULL);
+    for (int i=0; i < [params count]; i++) {
+        int rc = sqlite3_bind_text(stmt, i + 1, [[params objectAtIndex:i] UTF8String], -1, SQLITE_TRANSIENT);
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "error: %s\n", sqlite3_errmsg(self.db));
+        }
+    }
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+        NSString *key = [NSString stringWithUTF8String:sqlite3_column_text(stmt, 0)];
+        NSNumber *fileType = [NSNumber numberWithInt:sqlite3_column_int(stmt, 1) - 2];
+        [results setObject:fileType forKey:key];
+    }
+    sqlite3_finalize(stmt);
+    return results;
+}
+
+- (void) loadNameTypeCache {
+    NSString *query = @"select name, file_type_id from file_name";
+    NSDictionary<NSString*,NSNumber*>* nameTypeMap = [self getFileTypesForQuery:query andParams:[[NSArray alloc] init]];
+    [self.nameTypeCache addEntriesFromDictionary:nameTypeMap];
+    self.nameTypeCacheLoaded = true;
+}
+
+- (FileType) getFileTypeForQuery:(NSString*)query andParams:(NSArray<NSString*>*)params {
     FileType fileType = FileTypeUnknown;
     sqlite3_stmt *stmt;
     sqlite3_prepare_v2(self.db, [query UTF8String], -1, &stmt, NULL);
-    int rc = sqlite3_bind_text(stmt, 1, [elem UTF8String], -1, SQLITE_TRANSIENT);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "error: %s\n", sqlite3_errmsg(self.db));
+    for (int i=0; i < [params count]; i++) {
+        int rc = sqlite3_bind_text(stmt, i + 1, [[params objectAtIndex:i] UTF8String], -1, SQLITE_TRANSIENT);
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "error: %s\n", sqlite3_errmsg(self.db));
+        }
     }
     if (sqlite3_step(stmt) == SQLITE_ROW) {
         fileType = sqlite3_column_int(stmt, 0) - 2;
@@ -127,17 +159,30 @@
 }
 
 - (FileType) getFileTypeForFileName:(NSString *)fileName {
-    NSString *query = @"select file_type_id from file_name where name=?";
-    return [self getFileTypeForQuery:query andElem:fileName];
+    if (!self.nameTypeCacheLoaded) {
+        [self loadNameTypeCache];
+    }
+    if (self.nameTypeCache[fileName]) {
+        NSNumber* n = self.nameTypeCache[fileName];
+        return [n intValue];
+    }
+//    NSString *query = @"select file_type_id from file_name where name=?";
+//    NSArray<NSString*>* params = [NSArray arrayWithObject:fileName];
+//    return [self getFileTypeForQuery:query andParams:params];
+    return FileTypeUnknown;
 }
 
 - (FileType) getFileTypeForExtension:(NSString *)fileExt {
+    if (fileExt == nil || [fileExt isEqual:@""]) {
+        return FileTypeUnknown;
+    }
     if (self.extTypeCache[fileExt]) {
         NSNumber* n = self.extTypeCache[fileExt];
         return [n intValue];
     }
     NSString *query = @"select file_type_id from file_extension where extension=?";
-    FileType fileType = [self getFileTypeForQuery:query andElem:fileExt];
+    NSArray<NSString*>* params = [NSArray arrayWithObject:fileExt];
+    FileType fileType = [self getFileTypeForQuery:query andParams:params];
     [self.extTypeCache setObject:[NSNumber numberWithInt:fileType] forKey:fileExt];
     return fileType;
 }
