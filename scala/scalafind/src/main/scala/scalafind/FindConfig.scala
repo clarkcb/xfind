4631def79d6a35c77712e92f5e6c7fd31c2f8f09
Cary Clark <clarkcb@gmail.com>
2024-08-31 18:54:29 -0700

    Use ext, name caches in scala version

--- scala/scalafind/src/main/scala/scalafind/FindConfig.scala
+++ scala/scalafind/src/main/scala/scalafind/FindConfig.scala
@@ -1,10 +1,12 @@
 package scalafind
 
+import java.nio.file.{Path, Paths}
+
 object FindConfig {
-  val HOME: String = System.getProperty("user.home")
-  val XFINDPATH: String =  Option(System.getenv("XFIND_PATH")) match {
-      case Some(xfindPath) => xfindPath
-      case _ => HOME + "/src/xfind"
+//  val HOME: String = System.getProperty("user.home")
+  val XFIND_PATH: Path =  Option(System.getenv("XFIND_PATH")) match {
+      case Some(xfindPath) => Paths.get(xfindPath)
+      case _ => Paths.get(System.getProperty("user.home"), "src", "xfind")
   }
-  val XFINDDB: String = XFINDPATH + "/shared/xfind.db"
+  val XFIND_DB: Path = Paths.get(XFIND_PATH.toString, "shared", "xfind.db")
 }
