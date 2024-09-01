8f8777fc7309e0077d054b540dba32d9c070fb70
Cary Clark <clarkcb@gmail.com>
2024-08-31 18:54:29 -0700

    Use ext, name caches in scala version

--- scala/scalafind/src/main/scala/scalafind/FileTypes.scala
+++ scala/scalafind/src/main/scala/scalafind/FileTypes.scala
@@ -42,7 +42,9 @@ class FileTypes {
   private val SELECT_FILE_TYPE_ID_FOR_EXTENSION = "SELECT file_type_id FROM file_extension WHERE extension = ?"
 
   private var _conn: Option[Connection] = None
-  private var _fileTypeCache = Map[String, FileType]()
+  private var _extTypeCache = Map[String, FileType]()
+  private var _nameTypeCache = Map[String, FileType]()
+  private var _nameTypeCacheLoaded = false
 
   private def getConnection: Option[Connection] = {
     Class.forName("org.sqlite.JDBC")
@@ -52,7 +54,7 @@ class FileTypes {
         try {
           val config = SQLiteConfig()
           config.setReadOnly(true)
-          _conn = Some(DriverManager.getConnection("jdbc:sqlite:" + FindConfig.XFINDDB, config.toProperties))
+          _conn = Some(DriverManager.getConnection("jdbc:sqlite:" + FindConfig.XFIND_DB, config.toProperties))
           _conn
         } catch {
           case e: Exception =>
@@ -62,12 +64,40 @@ class FileTypes {
     }
   }
 
-  private def getFileTypeForQueryAndElem(query: String, elem: String): FileType = {
+  private def getFileTypesForQueryAndParams(query: String, params: List[String]): Map[String, FileType] = {
+    val results = mutable.Map[String, FileType]()
     getConnection match
       case Some(conn) =>
         try {
           val stmt = conn.prepareStatement(query)
-          stmt.setString(1, elem)
+          params.zipWithIndex.foreach { case (param, idx) => stmt.setString(idx + 1, param) }
+          val rs = stmt.executeQuery()
+          while (rs.next()) {
+            val key = rs.getString(1)
+            val fileTypeId = rs.getInt(2) - 1
+            results(key) = fileTypes(fileTypeId)
+          }
+          Map.empty[String, FileType] ++ results
+        } catch {
+          case e: SQLException =>
+            Common.logError(e.getMessage)
+            Map.empty[String, FileType] ++ results
+        }
+      case None =>
+        Map.empty[String, FileType] ++ results
+  }
+
+  private def loadNameTypeCache(): Unit = {
+    _nameTypeCache = getFileTypesForQueryAndParams("SELECT name, file_type_id FROM file_name", List.empty)
+    _nameTypeCacheLoaded = true
+  }
+
+  private def getFileTypeForQueryAndParams(query: String, params: List[String]): FileType = {
+    getConnection match
+      case Some(conn) =>
+        try {
+          val stmt = conn.prepareStatement(query)
+          params.zipWithIndex.foreach { case (param, idx) => stmt.setString(idx + 1, param) }
           val rs = stmt.executeQuery()
           if (rs.next()) {
             val fileTypeId = rs.getInt("file_type_id") - 1
@@ -85,13 +115,17 @@ class FileTypes {
   }
 
   private def getFileTypeForFileName(fileName: String): FileType = {
-    getFileTypeForQueryAndElem(SELECT_FILE_TYPE_ID_FOR_FILE_NAME, fileName)
+    if (!_nameTypeCacheLoaded) loadNameTypeCache()
+    if (_nameTypeCache.contains(fileName)) return _nameTypeCache(fileName)
+//    getFileTypeForQueryAndParams(SELECT_FILE_TYPE_ID_FOR_FILE_NAME, List(fileName))
+    FileType.Unknown
   }
 
   private def getFileTypeForExtension(fileExt: String): FileType = {
-    if (_fileTypeCache.contains(fileExt)) return _fileTypeCache(fileExt)
-    val fileType = getFileTypeForQueryAndElem(SELECT_FILE_TYPE_ID_FOR_EXTENSION, fileExt)
-    _fileTypeCache += (fileExt -> fileType)
+    if (fileExt.isBlank) return FileType.Unknown
+    if (_extTypeCache.contains(fileExt)) return _extTypeCache(fileExt)
+    val fileType = getFileTypeForQueryAndParams(SELECT_FILE_TYPE_ID_FOR_EXTENSION, List(fileExt))
+    _extTypeCache += (fileExt -> fileType)
     fileType
   }
 
