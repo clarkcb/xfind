93da9911b63a78e3c9fdbd28974ced3b6b8882d0
Cary Clark <clarkcb@gmail.com>
2024-12-06 14:25:28 -0800

    Add num arg processing to java version

--- java/javafind/src/main/java/javafind/FindOptions.java
+++ java/javafind/src/main/java/javafind/FindOptions.java
@@ -23,6 +23,8 @@ import java.util.*;
 public class FindOptions {
     private static final String FIND_OPTIONS_JSON_PATH = "/findoptions.json";
     private final List<FindOption> options;
+    private final Map<String, String> longArgMap = new HashMap<>();
+
 
     public FindOptions() throws IOException {
         options = new ArrayList<>();
@@ -30,44 +32,12 @@ public class FindOptions {
     }
 
     @FunctionalInterface
-    private interface ArgSetter {
-        void set(String s, FindSettings settings);
-    }
-
-    private final int actionMapSize = 21;
-    private final Map<String, ArgSetter> argActionMap = new HashMap<>(actionMapSize) {
-        {
-            put("in-archiveext", (s, settings) -> settings.addInArchiveExtension(s));
-            put("in-archivefilepattern", (s, settings) -> settings.addInArchiveFilePattern(s));
-            put("in-dirpattern", (s, settings) -> settings.addInDirPattern(s));
-            put("in-ext", (s, settings) -> settings.addInExtension(s));
-            put("in-filepattern", (s, settings) -> settings.addInFilePattern(s));
-            put("in-filetype", (s, settings) -> settings.addInFileType(s));
-            put("maxdepth", (s, settings) -> settings.setMaxDepth(Integer.parseInt(s)));
-            put("maxlastmod", (s, settings) -> settings.setMaxLastMod(s));
-            put("maxsize", (s, settings) -> settings.setMaxSize(Integer.parseInt(s)));
-            put("mindepth", (s, settings) -> settings.setMinDepth(Integer.parseInt(s)));
-            put("minlastmod", (s, settings) -> settings.setMinLastMod(s));
-            put("minsize", (s, settings) -> settings.setMinSize(Integer.parseInt(s)));
-            put("out-archiveext", (s, settings) -> settings.addOutArchiveExtension(s));
-            put("out-archivefilepattern", (s, settings) -> settings.addOutArchiveFilePattern(s));
-            put("out-dirpattern", (s, settings) -> settings.addOutDirPattern(s));
-            put("out-ext", (s, settings) -> settings.addOutExtension(s));
-            put("out-filepattern", (s, settings) -> settings.addOutFilePattern(s));
-            put("out-filetype", (s, settings) -> settings.addOutFileType(s));
-            put("path", (s, settings) -> settings.addPath(s));
-            put("settings-file", (s, settings) -> settingsFromFilePath(s, settings));
-            put("sort-by", (s, settings) -> settings.setSortBy(SortBy.forName(s)));
-        }
-    };
-
-    @FunctionalInterface
-    private interface BooleanFlagSetter {
+    private interface BooleanSetter {
         void set(Boolean b, FindSettings settings);
     }
 
-    private final int flagMapSize = 18;
-    private final Map<String, BooleanFlagSetter> boolflagActionMap = new HashMap<>(flagMapSize) {
+    private final int boolActionMapSize = 18;
+    private final Map<String, BooleanSetter> boolActionMap = new HashMap<>(boolActionMapSize) {
         {
             put("archivesonly", (b, settings) -> settings.setArchivesOnly(b));
             put("debug", (b, settings) -> settings.setDebug(b));
@@ -92,6 +62,60 @@ public class FindOptions {
         }
     };
 
+    @FunctionalInterface
+    private interface StringSetter {
+        void set(String s, FindSettings settings);
+    }
+
+    private final int stringActionMapSize = 17;
+    private final Map<String, StringSetter> stringActionMap = new HashMap<>(stringActionMapSize) {
+        {
+            put("in-archiveext", (s, settings) -> settings.addInArchiveExtension(s));
+            put("in-archivefilepattern", (s, settings) -> settings.addInArchiveFilePattern(s));
+            put("in-dirpattern", (s, settings) -> settings.addInDirPattern(s));
+            put("in-ext", (s, settings) -> settings.addInExtension(s));
+            put("in-filepattern", (s, settings) -> settings.addInFilePattern(s));
+            put("in-filetype", (s, settings) -> settings.addInFileType(s));
+            put("maxlastmod", (s, settings) -> settings.setMaxLastMod(s));
+            put("minlastmod", (s, settings) -> settings.setMinLastMod(s));
+            put("out-archiveext", (s, settings) -> settings.addOutArchiveExtension(s));
+            put("out-archivefilepattern", (s, settings) -> settings.addOutArchiveFilePattern(s));
+            put("out-dirpattern", (s, settings) -> settings.addOutDirPattern(s));
+            put("out-ext", (s, settings) -> settings.addOutExtension(s));
+            put("out-filepattern", (s, settings) -> settings.addOutFilePattern(s));
+            put("out-filetype", (s, settings) -> settings.addOutFileType(s));
+            put("path", (s, settings) -> settings.addPath(s));
+            // put("settings-file", (s, settings) -> settingsFromFilePath(s, settings));
+            put("sort-by", (s, settings) -> settings.setSortBy(SortBy.forName(s)));
+        }
+    };
+
+    @FunctionalInterface
+    private interface IntegerSetter {
+        void set(Integer i, FindSettings settings);
+    }
+
+    private final int intActionMapSize = 2;
+    private final Map<String, IntegerSetter> intActionMap = new HashMap<>(intActionMapSize) {
+        {
+            put("maxdepth", (i, settings) -> settings.setMaxDepth(i));
+            put("mindepth", (i, settings) -> settings.setMinDepth(i));
+        }
+    };
+
+    @FunctionalInterface
+    private interface LongSetter {
+        void set(Long l, FindSettings settings);
+    }
+
+    private final int longActionMapSize = 2;
+    private final Map<String, LongSetter> longActionMap = new HashMap<>(longActionMapSize) {
+        {
+            put("maxsize", (l, settings) -> settings.setMaxSize(l));
+            put("minsize", (l, settings) -> settings.setMinSize(l));
+        }
+    };
+
     private void setOptionsFromJson() {
         var findOptionsInputStream = getClass().getResourceAsStream(FIND_OPTIONS_JSON_PATH);
         assert findOptionsInputStream != null;
@@ -101,33 +125,31 @@ public class FindOptions {
         for (int i=0; i<findOptionsArray.length(); i++) {
             var findOptionObj = findOptionsArray.getJSONObject(i);
             var longArg = findOptionObj.getString("long");
+            longArgMap.put(longArg, longArg);
             var desc = findOptionObj.getString("desc");
             var shortArg = "";
             if (findOptionObj.has("short")) {
                 shortArg = findOptionObj.getString("short");
+                longArgMap.put(shortArg, longArg);
             }
             options.add(new FindOption(shortArg, longArg, desc));
         }
     }
 
-    private void settingsFromFilePath(final String filePath, FindSettings settings) {
+    private void settingsFromFilePath(final String filePath, FindSettings settings) throws FindException {
         var path = Paths.get(filePath);
         try {
             if (!Files.exists(path)) {
-                Logger.log("Settings file not found: " + filePath);
-                System.exit(1);
+                throw new FindException("Settings file not found: " + filePath);
             }
             if (!FileUtil.hasExtension(filePath, "json")) {
-                Logger.log("Invalid settings file type (must be JSON): " + filePath);
-                System.exit(1);
+                throw new FindException("Invalid settings file type (must be JSON): " + filePath);
             }
             settingsFromJson(FileUtil.getFileContents(path), settings);
         } catch (FileNotFoundException e) {
-            Logger.log("Settings file not found: " + filePath);
-            System.exit(1);
+            throw new FindException("Settings file not found: " + filePath);
         } catch (IOException e) {
-            Logger.log("IOException reading settings file: " + filePath);
-            System.exit(1);
+            throw new FindException("IOException reading settings file: " + filePath);
         }
     }
 
@@ -140,23 +162,34 @@ public class FindOptions {
     }
 
     private void applySetting(final String arg, final Object obj, FindSettings settings) {
-        if (obj instanceof String) {
-            try {
-                applySetting(arg, (String)obj, settings);
-            } catch (FindException e) {
-                Logger.logError("FindException: " + e.getMessage());
-            }
-        } else if (obj instanceof Boolean) {
+        if (obj instanceof Boolean) {
             try {
                 applySetting(arg, (Boolean)obj, settings);
             } catch (FindException e) {
                 Logger.logError("FindException: " + e.getMessage());
             }
-        } else if ((obj instanceof Integer) || (obj instanceof Long)) {
+        } else if (obj instanceof String) {
             try {
-                applySetting(arg, obj.toString(), settings);
+                applySetting(arg, (String)obj, settings);
             } catch (FindException e) {
                 Logger.logError("FindException: " + e.getMessage());
+            }
+        } else if (obj instanceof Integer || obj instanceof Long) {
+            // these are handled together because the json parser might return an Integer or Long
+            if (intActionMap.containsKey(arg)) {
+                try {
+                    applySetting(arg, (Integer)obj, settings);
+                } catch (FindException e) {
+                    Logger.logError("FindException: " + e.getMessage());
+                }
+            } else if (longActionMap.containsKey(arg)) {
+                try {
+                    applySetting(arg, (Long)obj, settings);
+                } catch (FindException e) {
+                    Logger.logError("FindException: " + e.getMessage());
+                }
+            } else {
+                Logger.logError("Invalid option: " + arg);
             }
         } else if (obj instanceof JSONArray) {
             for (int i=0; i < ((JSONArray)obj).length(); i++) {
@@ -167,10 +200,19 @@ public class FindOptions {
         }
     }
 
+    private void applySetting(final String arg, final Boolean val, FindSettings settings)
+            throws FindException{
+        if (this.boolActionMap.containsKey(arg)) {
+            this.boolActionMap.get(arg).set(val, settings);
+        } else {
+            throw new FindException("Invalid option: " + arg);
+        }
+    }
+
     private void applySetting(final String arg, final String val, FindSettings settings)
             throws FindException {
-        if (this.argActionMap.containsKey(arg)) {
-            this.argActionMap.get(arg).set(val, settings);
+        if (this.stringActionMap.containsKey(arg)) {
+            this.stringActionMap.get(arg).set(val, settings);
         } else if (arg.equals("path")) {
             settings.addPath(val);
         } else {
@@ -178,10 +220,19 @@ public class FindOptions {
         }
     }
 
-    private void applySetting(final String arg, final Boolean val, FindSettings settings)
+    private void applySetting(final String arg, final Integer val, FindSettings settings)
             throws FindException{
-        if (this.boolflagActionMap.containsKey(arg)) {
-            this.boolflagActionMap.get(arg).set(val, settings);
+        if (this.intActionMap.containsKey(arg)) {
+            this.intActionMap.get(arg).set(val, settings);
+        } else {
+            throw new FindException("Invalid option: " + arg);
+        }
+    }
+
+    private void applySetting(final String arg, final Long val, FindSettings settings)
+            throws FindException{
+        if (this.longActionMap.containsKey(arg)) {
+            this.longActionMap.get(arg).set(val, settings);
         } else {
             throw new FindException("Invalid option: " + arg);
         }
@@ -192,31 +243,34 @@ public class FindOptions {
         // default printFiles to true since running from command line
         settings.setPrintFiles(true);
 
-        // add short arg mappings
-        options.stream().filter(o -> !o.getShortArg().isEmpty()).forEach(o -> {
-            if (argActionMap.containsKey(o.getLongArg())) {
-                argActionMap.put(o.getShortArg(), argActionMap.get(o.getLongArg()));
-            } else if (boolflagActionMap.containsKey(o.getLongArg())) {
-                boolflagActionMap.put(o.getShortArg(), boolflagActionMap.get(o.getLongArg()));
-            }
-        });
-
         Queue<String> queue = new LinkedList<>(Arrays.asList(args));
         while (!queue.isEmpty()) {
-            String arg = queue.remove();
+            var arg = queue.remove();
             if (arg.startsWith("-")) {
                 while (arg.startsWith("-")) {
                     arg = arg.substring(1);
                 }
-                if (this.argActionMap.containsKey(arg)) {
+                var longArg = longArgMap.get(arg);
+                if (this.boolActionMap.containsKey(longArg)) {
+                    this.boolActionMap.get(longArg).set(true, settings);
+                } else if (this.stringActionMap.containsKey(longArg)
+                        || this.intActionMap.containsKey(longArg)
+                        || this.longActionMap.containsKey(longArg)
+                        || longArg.equals("settings-file")) {
                     if (!queue.isEmpty()) {
                         String argVal = queue.remove();
-                        this.argActionMap.get(arg).set(argVal, settings);
+                        if (this.stringActionMap.containsKey(longArg)) {
+                            this.stringActionMap.get(longArg).set(argVal, settings);
+                        } else if (this.intActionMap.containsKey(longArg)) {
+                            this.intActionMap.get(longArg).set(Integer.parseInt(argVal), settings);
+                        } else if (this.longActionMap.containsKey(longArg)) {
+                            this.longActionMap.get(longArg).set(Long.parseLong(argVal), settings);
+                        } else if (longArg.equals("settings-file")) {
+                            settingsFromFilePath(argVal, settings);
+                        }
                     } else {
                         throw new FindException("Missing value for option " + arg);
                     }
-                } else if (this.boolflagActionMap.containsKey(arg)) {
-                    this.boolflagActionMap.get(arg).set(true, settings);
                 } else {
                     throw new FindException("Invalid option: " + arg);
                 }
