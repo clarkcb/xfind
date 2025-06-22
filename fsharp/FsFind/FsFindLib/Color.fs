f82b2227d8216438473c5ef3cdf1fe6b4a073811
Cary Clark <clarkcb@gmail.com>
2025-06-22 14:17:37 -0700

    Improve colorize in F# version

--- fsharp/FsFind/FsFindLib/Color.fs
+++ fsharp/FsFind/FsFindLib/Color.fs
@@ -10,4 +10,20 @@ module Color =
     let Purple = "\u001B[35m"
     let Cyan =   "\u001B[36m"
     let White =  "\u001B[37m"
+
+    let Colorize (s : string) (matchStartIndex : int) (matchEndIndex : int) : string =
+        let prefix =
+            if matchStartIndex > 0
+            then s.Substring(0, matchStartIndex)
+            else ""
+        let suffix =
+            if matchEndIndex < s.Length
+            then s.Substring(matchEndIndex)
+            else ""
+        let matchLength = matchEndIndex - matchStartIndex
+        prefix +
+            Green + 
+            s.Substring(matchStartIndex, matchLength) +
+            Reset + 
+            suffix
 ;;
--- fsharp/FsFind/FsFindLib/FileResultFormatter.fs
+++ fsharp/FsFind/FsFindLib/FileResultFormatter.fs
@@ -5,22 +5,6 @@ open System.Text.RegularExpressions
 
 type FileResultFormatter (settings : FindSettings) =
 
-    let Colorize (s : string) (matchStartIndex : int) (matchEndIndex : int) : string =
-        let prefix =
-            if matchStartIndex > 0
-            then s.Substring(0, matchStartIndex)
-            else ""
-        let suffix =
-            if matchEndIndex < s.Length
-            then s.Substring(matchEndIndex)
-            else ""
-        let matchLength = matchEndIndex - matchStartIndex
-        prefix +
-            Color.Green + 
-            s.Substring(matchStartIndex, matchLength) +
-            Color.Reset + 
-            suffix
-
     member this.FormatDirectoryWithColor (dir : DirectoryInfo) : string =
         if dir = null
         then "."
@@ -29,33 +13,39 @@ type FileResultFormatter (settings : FindSettings) =
             match (Seq.tryFind (fun p -> (p:Regex).Match(dirName).Success) settings.InDirPatterns) with
             | Some dirPattern ->
                 let dirMatch = dirPattern.Match(dirName)
-                Colorize dirName dirMatch.Index (dirMatch.Index + dirMatch.Length)
+                Color.Colorize dirName dirMatch.Index (dirMatch.Index + dirMatch.Length)
             | None -> dirName
 
+    member this.FormatDirectoryFun =
+        if settings.Colorize
+        then this.FormatDirectoryWithColor
+        else fun (dir : DirectoryInfo) -> dir.ToString()
+
     member this.FormatDirectory (dir : DirectoryInfo) : string =
-        if settings.Colorize && not (List.isEmpty settings.InDirPatterns)
-        then this.FormatDirectoryWithColor dir
-        else dir.ToString()
+        this.FormatDirectoryFun dir
 
     member this.FormatFileNameWithColor (fileName : string) : string =
         let formattedFileName =
             match (Seq.tryFind (fun p -> (p:Regex).Match(fileName).Success) settings.InFilePatterns) with
             | Some filePattern ->
                 let fileMatch = filePattern.Match(fileName)
-                Colorize fileName fileMatch.Index (fileMatch.Index + fileMatch.Length)
+                Color.Colorize fileName fileMatch.Index (fileMatch.Index + fileMatch.Length)
             | None -> fileName
         if List.isEmpty settings.InExtensions
         then formattedFileName
         else
             let idx = formattedFileName.LastIndexOf('.')
             if idx > 0 && idx < formattedFileName.Length - 1
-            then Colorize formattedFileName (idx + 1) formattedFileName.Length
+            then Color.Colorize formattedFileName (idx + 1) formattedFileName.Length
             else formattedFileName
 
+    member this.FormatFileNameFun =
+        if settings.Colorize
+        then this.FormatFileNameWithColor
+        else fun (fileName : string) -> fileName
+
     member this.FormatFileName (fileName : string) : string =
-        if settings.Colorize && (not (List.isEmpty settings.InExtensions) || not (List.isEmpty settings.InFilePatterns))
-        then this.FormatFileNameWithColor fileName
-        else fileName
+        this.FormatFileNameFun fileName
 
     member this.FormatFileResult (result : FileResult.t) : string =
         let parent = this.FormatDirectory result.File.Directory
