5bb5fae64adf51ab0feebf50b48fa36eccd65939
Cary Clark <clarkcb@gmail.com>
2026-01-11 12:25:08 -0800

    Version upgrades

--- elixir/exfind/mix.exs
+++ elixir/exfind/mix.exs
@@ -5,7 +5,7 @@ defmodule ExFind.MixProject do
     [
       app: :exfind,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.19",
       escript: [main_module: ExFind.App, path: "./bin/exfind"],
       # start_permanent: Mix.env() == :prod,
       start_permanent: true,
