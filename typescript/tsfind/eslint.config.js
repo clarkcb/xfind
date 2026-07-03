86553e4bcf4ae6bf6e66993f8e5aa68b31bde359
Cary Clark <clarkcb@gmail.com>
2026-07-03 11:38:24 -0700

    More reformatting in ts version

--- typescript/tsfind/eslint.config.js
+++ typescript/tsfind/eslint.config.js
@@ -1,35 +1,35 @@
-import tsParser from "@typescript-eslint/parser";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import prettierPlugin from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
   {
     // Apply to all TypeScript and JavaScript files
-    files: ["**/*.{ts,tsx,js,jsx}"],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       parser: tsParser,
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
     plugins: {
-      "@typescript-eslint": tsPlugin,
-      "prettier": prettierPlugin,
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
     },
     rules: {
       // Include recommended TypeScript rules
       ...tsPlugin.configs.recommended.rules,
       // Run Prettier as an ESLint rule and report differences as errors
-      "prettier/prettier": "error",
+      'prettier/prettier': 'error',
       // Custom overrides
-      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-      "no-console": "warn",
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'warn',
     },
   },
   // Turns off all ESLint rules that conflict with Prettier
   prettierConfig,
   {
     // Ignore build folders and configuration files
-    ignores: ["dist/", "node_modules/", "eslint.config.js"],
-  }
+    ignores: ['dist/', 'node_modules/', 'eslint.config.js'],
+  },
 ];
