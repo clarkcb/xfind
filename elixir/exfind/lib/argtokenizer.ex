ffdfdbf4d84da940249cd5e76db7f02adc6b678a
Cary Clark <clarkcb@gmail.com>
2026-08-08 14:54:21 -0700

    Add support for size modifiers in elixir version

--- elixir/exfind/lib/argtokenizer.ex
+++ elixir/exfind/lib/argtokenizer.ex
@@ -15,13 +15,20 @@ defmodule ExFind.ArgTokenizer do
   """
   alias ExFind.ArgToken
   alias ExFind.FileUtil
+  alias ExFind.FindError
   alias ExFind.StringUtil
 
   require OptionParser
 
+  @num_modifier_regex ~r/^\d+([ckmgtp])$/i
+
   defstruct options: [], bool_opts: [], str_opts: [], int_opts: []
 
   def new(args), do: __struct__(args)
+
+  def parsed_string_arg_to_token(arg_name, str_value) do
+    ArgToken.new(name: arg_name, arg_type: :string, value: str_value)
+  end
 
   def parsed_args_to_tokens(parsed_args, arg_tokenizer, tokens) do
     case parsed_args do
@@ -53,6 +60,60 @@ defmodule ExFind.ArgTokenizer do
     String.replace_leading(option, "-", "")
   end
 
+  def parse_size!(size) do
+    num_modifier_regex = @num_modifier_regex
+    cond do
+      is_integer(size) -> size
+      is_binary(size) and Regex.match?(num_modifier_regex, size) ->
+        multiplier = case String.downcase(String.slice(size, -1..-1//1)) do
+          "c" -> 1
+          "k" -> 1024
+          "m" -> 1024 * 1024
+          "g" -> 1024 * 1024 * 1024
+          "t" -> 1024 * 1024 * 1024 * 1024
+          "p" -> 1024 * 1024 * 1024 * 1024 * 1024
+        end
+        num_part = String.slice(size, 0..-2//1) |> String.to_integer()
+        num_part * multiplier
+      true -> raise FindError, message: "Invalid size: #{size}"
+    end
+  end
+
+  def parse_size(size) do
+    try do
+      parsed_size = parse_size!(size)
+      {:ok, parsed_size}
+    rescue
+      e in FindError -> {:error, e.message}
+    end
+  end
+
+  def process_invalid_args(invalid_args, arg_tokenizer, processed_args) do
+    case invalid_args do
+      [] -> {:ok, processed_args}
+      [{opt, "(-1)"} | rest] -> process_invalid_args(rest, arg_tokenizer, processed_args ++ [{String.to_atom(String.slice(opt, 2..-1//1)), -1}])
+      [{"--maxsize", arg_val} | rest] ->
+        case parse_size(arg_val) do
+          {:ok, size} -> process_invalid_args(rest, arg_tokenizer, processed_args ++ [{:maxsize, size}])
+          {:error, _e} -> {:error, "Invalid value for option maxsize: #{arg_val}"}
+        end
+      [{"--minsize", arg_val} | rest] ->
+        case parse_size(arg_val) do
+          {:ok, size} -> process_invalid_args(rest, arg_tokenizer, processed_args ++ [{:minsize, size}])
+          {:error, _e} -> {:error, "Invalid value for option minsize: #{arg_val}"}
+        end
+      [{opt, _} | _rest] -> {:error, "Invalid option: #{trim_option(opt)}"}
+    end
+  end
+
+  def tokenize_size_arg(arg_name, str_value) do
+    IO.puts("tokenize_size_arg(#{arg_name}, #{str_value})")
+    case parse_size(str_value) do
+      {:ok, size} -> {:ok, [ArgToken.new(name: arg_name, arg_type: :integer, value: size)]}
+      {:error, _e} -> {:error, "Invalid size option: #{arg_name}, value: #{str_value}"}
+    end
+  end
+
   def tokenize_args(args, arg_tokenizer) do
     bool_opts = arg_tokenizer.bool_opts |> Enum.map(fn o -> {o, :boolean} end)
     int_opts = arg_tokenizer.int_opts |> Enum.map(fn o -> {o, :integer} end)
@@ -65,11 +126,9 @@ defmodule ExFind.ArgTokenizer do
                  |> Enum.map(fn o -> {String.to_atom(o.short_arg), String.to_atom(String.replace(o.long_arg, "-", "_"))} end)
     {parsed_args, paths, invalid} = OptionParser.parse(args, strict: parser_opts, aliases: alias_opts)
     parsed_args_with_paths = parsed_args ++ Enum.map(paths, fn p -> {:path, p} end)
-
-    case invalid do
-      [{opt, "(-1)"} | _rest] -> parsed_args_to_tokens(parsed_args_with_paths ++ [{String.to_atom(String.slice(opt, 2..-1//1)), -1}], arg_tokenizer, [])
-      [{opt, nil} | _rest] -> {:error, "Invalid option: #{trim_option(opt)}, rest: #{_rest}"}
-      [] -> parsed_args_to_tokens(parsed_args_with_paths, arg_tokenizer, [])
+    case process_invalid_args(invalid, arg_tokenizer, []) do
+      {:ok, processed_invalid_args} -> parsed_args_to_tokens(parsed_args_with_paths ++ processed_invalid_args, arg_tokenizer, [])
+      {:error, _e} = error -> error
     end
   end
 
