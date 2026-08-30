509c011de545e479f132f6cd310eb4da4119684d
Cary Clark <clarkcb@gmail.com>
2026-08-30 10:00:24 -0700

    Add XFIND_CONFIG_DIR to python version

--- python/pyfind/pyfind/findconfig.py
+++ python/pyfind/pyfind/findconfig.py
@@ -29,8 +29,10 @@ class FindConfig:
         # FINDOPTIONS_PATH = os.path.join(data_path, 'findoptions.json')
         FIND_OPTIONS_PATH = _data.joinpath('findoptions.json')
 
-        HOME = os.getenv('HOME')
-        DEFAULT_SETTINGS_PATH = os.path.join(HOME, '.config', 'xfind', 'settings.json')
+        HOME = os.getenv('HOME', '')
+        DEFAULT_FIND_CONFIG_DIR = os.path.join(HOME, '.config', 'xfind')
+        XFIND_CONFIG_DIR = os.getenv('XFIND_CONFIG_DIR', DEFAULT_FIND_CONFIG_DIR)
+        DEFAULT_SETTINGS_PATH = os.path.join(XFIND_CONFIG_DIR, 'settings.json')
 
         self.file_types_path = FILE_TYPES_PATH
         self.find_options_path = FIND_OPTIONS_PATH
