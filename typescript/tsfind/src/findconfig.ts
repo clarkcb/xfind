9c1663c85fe37e8e0787d6cf277e613cf45242fe
Cary Clark <clarkcb@gmail.com>
2026-08-30 10:02:48 -0700

    Add XFIND_CONFIG_DIR to ts version

--- typescript/tsfind/src/findconfig.ts
+++ typescript/tsfind/src/findconfig.ts
@@ -20,14 +20,17 @@ export class FindConfig {
   defaultFindSettingsPath: string;
 
   constructor() {
-    const xfindPath = process.env.XFIND_PATH
-      ? process.env.XFIND_PATH
-      : path.join(HOME, 'src', 'xfind');
+    const defaultXFindConfigDir = path.join(HOME, '.config', 'xfind');
+    const xfindConfigDir = process.env.XFIND_CONFIG_DIR
+      ? process.env.XFIND_CONFIG_DIR
+      : defaultXFindConfigDir;
+    const defaultXFindPath = path.join(HOME, 'src', 'xfind');
+    const xfindPath = process.env.XFIND_PATH ? process.env.XFIND_PATH : defaultXFindPath;
     const jsfindPath = path.join(xfindPath, 'javascript', 'jsfind');
     const dataPath = path.join(jsfindPath, 'data');
     const fileTypesPath = path.join(dataPath, 'filetypes.json');
     const findOptionsPath = path.join(dataPath, 'findoptions.json');
-    const defaultFindSettingsPath = path.join(HOME, '.config', 'xfind', 'settings.json');
+    const defaultFindSettingsPath = path.join(xfindConfigDir, 'settings.json');
 
     this.xfindPath = xfindPath;
     this.fileTypesPath = fileTypesPath;
