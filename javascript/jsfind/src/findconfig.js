a9bb501d76baadd5acfb891b8ab636cfabcf21db
Cary Clark <clarkcb@gmail.com>
2026-08-30 09:56:30 -0700

    Add XFIND_CONFIG_DIR to js version

--- javascript/jsfind/src/findconfig.js
+++ javascript/jsfind/src/findconfig.js
@@ -15,15 +15,17 @@ const HOME = process.env[HOME_NAME];
 
 class FindConfig {
   constructor() {
-    const xfindPath = process.env.XFIND_PATH
-      ? process.env.XFIND_PATH
-      : path.join(HOME, 'src', 'xfind');
-    // var sharedPath = path.join(xfind_path, 'shared');
+    const defaultXFindConfigDir = path.join(HOME, '.config', 'xfind');
+    const xfindConfigDir = process.env.XFIND_CONFIG_DIR
+      ? process.env.XFIND_CONFIG_DIR
+      : defaultXFindConfigDir;
+    const defaultXFindPath = path.join(HOME, 'src', 'xfind');
+    const xfindPath = process.env.XFIND_PATH ? process.env.XFIND_PATH : defaultXFindPath;
     const jsfindPath = path.join(xfindPath, 'javascript', 'jsfind');
     const dataPath = path.join(jsfindPath, 'data');
     const fileTypesPath = path.join(dataPath, 'filetypes.json');
     const findOptionsPath = path.join(dataPath, 'findoptions.json');
-    const defaultFindSettingsPath = path.join(HOME, '.config', 'xfind', 'settings.json');
+    const defaultFindSettingsPath = path.join(xfindConfigDir, 'settings.json');
 
     this.xfindPath = xfindPath;
     this.fileTypesPath = fileTypesPath;
