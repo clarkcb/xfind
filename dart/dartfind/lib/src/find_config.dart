db788ee2860768e99dcf7ef4ea3259f6e2ced3a6
Cary Clark <clarkcb@gmail.com>
2026-08-30 09:51:55 -0700

    Add XFIND_CONFIG_DIR to dart version

--- dart/dartfind/lib/src/find_config.dart
+++ dart/dartfind/lib/src/find_config.dart
@@ -1,19 +1,23 @@
 import 'dart:io' show Platform;
 
 class FindConfig {
+  String xFindConfigDir = "";
   String xFindPath = "";
   String fileTypesPath = "";
   String findOptionsPath = "";
   String defaultFindSettingsPath = "";
 
   FindConfig() {
+    xFindConfigDir = Platform.environment.containsKey('XFIND_CONFIG_DIR')
+        ? Platform.environment['XFIND_CONFIG_DIR']!
+        : '${Platform.environment['HOME']!}/.config/xfind';
+
     xFindPath = Platform.environment.containsKey('XFIND_PATH')
         ? Platform.environment['XFIND_PATH']!
         : '${Platform.environment['HOME']!}/src/xfind';
 
     fileTypesPath = '$xFindPath/shared/filetypes.json';
     findOptionsPath = '$xFindPath/shared/findoptions.json';
-    defaultFindSettingsPath =
-        '${Platform.environment['HOME']!}/.config/xfind/settings.json';
+    defaultFindSettingsPath = '${xFindConfigDir}/settings.json';
   }
 }
