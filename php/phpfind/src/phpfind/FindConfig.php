28009847868926b7cdf5a85a7a4035d4a0b5ab94
Cary Clark <clarkcb@gmail.com>
2026-08-30 09:59:23 -0700

    Add XFIND_CONFIG_DIR to php version

--- php/phpfind/src/phpfind/FindConfig.php
+++ php/phpfind/src/phpfind/FindConfig.php
@@ -16,11 +16,15 @@ readonly class FindConfig
     public function __construct()
     {
         $home = getenv('HOME');
+        $xfind_config_dir = getenv('XFIND_CONFIG_DIR');
+        if (!$xfind_config_dir) {
+            $xfind_config_dir = FileUtil::join_paths($home, '.config', 'xfind');
+        }
 
         $resources_path = FileUtil::join_paths(__DIR__, '..', '..', 'resources');
         $file_types_path = FileUtil::join_paths($resources_path, 'filetypes.json');
         $find_options_path = FileUtil::join_paths($resources_path, 'findoptions.json');
-        $default_find_settings_path = FileUtil::join_paths($home, '.config', 'xfind', 'settings.json');
+        $default_find_settings_path = FileUtil::join_paths($xfind_config_dir, 'settings.json');
 
         $this->file_types_path = $file_types_path;
         $this->find_options_path = $find_options_path;
