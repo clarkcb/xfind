e98c582c208945fdb311151b263ec1955bf36d63
Cary Clark <clarkcb@gmail.com>
2026-08-30 09:58:07 -0700

    Add XFIND_CONFIG_DIR to objc version

--- objc/objcfind/Sources/objcfind/FindConfig.m
+++ objc/objcfind/Sources/objcfind/FindConfig.m
@@ -6,14 +6,20 @@
     self = [super init];
     if (self) {
         NSString *homePath = [[[NSProcessInfo processInfo] environment] objectForKey:@"HOME"];
+        NSString *defaultXFindConfigDir = [NSString pathWithComponents:@[homePath, @".config", @"xfind"]];
+        NSString *xfindConfigDir = [[[NSProcessInfo processInfo] environment] objectForKey:@"XFIND_CONFIG_DIR"];
+        if (xfindConfigDir == nil) {
+            xfindConfigDir = defaultXFindConfigDir;
+        }
+        NSString *defaultXFindPath = [NSString pathWithComponents:@[homePath, @"src", @"xfind"]];
         NSString *xfindPath = [[[NSProcessInfo processInfo] environment] objectForKey:@"XFIND_PATH"];
         if (xfindPath == nil) {
-            xfindPath = [NSString pathWithComponents:@[homePath, @"src", @"xfind"]];
+            xfindPath = defaultXFindPath;
         }
         self.xfindPath = xfindPath;
         self.fileTypesPath = [NSString pathWithComponents:@[xfindPath, @"shared", @"filetypes.json"]];
         self.findOptionsPath = [NSString pathWithComponents:@[xfindPath, @"shared", @"findoptions.json"]];
-        self.defaultFindSettingsPath = [NSString pathWithComponents:@[homePath, @".config", @"xfind", @"settings.json"]];
+        self.defaultFindSettingsPath = [NSString pathWithComponents:@[xfindConfigDir, @"settings.json"]];
     }
     return self;
 }
