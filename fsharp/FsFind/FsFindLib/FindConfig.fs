fc5e3532607945ad7d5f4434d234a03bc01c53fb
Cary Clark <clarkcb@gmail.com>
2026-08-30 09:53:56 -0700

    Add XFIND_CONFIG_DIR to F# version

--- fsharp/FsFind/FsFindLib/FindConfig.fs
+++ fsharp/FsFind/FsFindLib/FindConfig.fs
@@ -1,7 +1,16 @@
 namespace FsFindLib
+open System
 open System.IO
 
 type FindConfig() =
+    let getXfindConfigDir () =
+        let defaultXFindConfigDir = Path.Join(FileUtil.GetHomePath(), ".config", "xfind")
+        let xFindConfigDir = Environment.GetEnvironmentVariable("XFIND_CONFIG_DIR")
+        if String.IsNullOrEmpty(xFindConfigDir) then
+            defaultXFindConfigDir
+        else
+            xFindConfigDir
+
     member val FileTypesPath : string = "FsFindLib.Resources.filetypes.json" with get, set
     member val FindOptionsPath : string = "FsFindLib.Resources.findoptions.json" with get, set
-    member val DefaultFindSettingsPath : string = Path.Join(FileUtil.GetHomePath(), ".config", "xfind", "settings.json") with get, set
+    member val DefaultFindSettingsPath : string = Path.Join(getXfindConfigDir(), "settings.json") with get, set
