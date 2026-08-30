55dbdef4437fb9c45a8de501ae21a81d4175daa2
Cary Clark <clarkcb@gmail.com>
2026-08-30 10:01:25 -0700

    Add XFIND_CONFIG_DIR to rust version

--- rust/rsfind/src/findconfig.rs
+++ rust/rsfind/src/findconfig.rs
@@ -15,6 +15,7 @@ pub struct FindConfig {
 
 #[derive(Serialize, Deserialize)]
 pub struct JsonConfig {
+    xfindconfigdir: String,
     xfindpath: String,
     version: String,
 }
@@ -23,20 +24,24 @@ pub const VERSION: &str = "1.0.0";
 
 impl FindConfig {
     pub fn new() -> FindConfig {
+        let default_xfind_config_dir: String = env::var("HOME").unwrap() + "/.config/xfind";
+        let xfind_config_dir: String = env::var("XFIND_CONFIG_DIR")
+            .unwrap_or_else(|_error| default_xfind_config_dir);
+        let default_xfind_path: String = env::var("HOME").unwrap() + "/src/xfind";
         let xfind_path: String = env::var("XFIND_PATH")
-            .unwrap_or_else(|_error| env::var("HOME").unwrap() + "/src/xfind");
+            .unwrap_or_else(|_error| default_xfind_path);
         let version = String::from(VERSION);
-        FindConfig::for_values(xfind_path, version)
+        FindConfig::for_values(xfind_config_dir, xfind_path, version)
     }
 
-    pub fn for_values(xfind_path: String, version: String) -> FindConfig {
+    pub fn for_values(xfind_config_dir: String, xfind_path: String, version: String) -> FindConfig {
         let shared_path = xfind_path.clone() + "/shared";
         FindConfig {
             xfind_path: xfind_path.clone(),
             shared_path: shared_path.clone(),
             file_types_path: shared_path.clone() + "/filetypes.json",
             find_options_path: shared_path.clone() + "/findoptions.json",
-            default_find_settings_path: env::var("HOME").unwrap() + "/.config/xfind/settings.json",
+            default_find_settings_path: xfind_config_dir.clone() + "/settings.json",
             version,
         }
     }
@@ -45,6 +50,6 @@ impl FindConfig {
         let contents = fs::read_to_string(json_file_path)
             .expect("Something went wrong reading the config file");
         let json_config: JsonConfig = serde_json::from_str(&contents).unwrap();
-        FindConfig::for_values(json_config.xfindpath, json_config.version)
+        FindConfig::for_values(json_config.xfindconfigdir, json_config.xfindpath, json_config.version)
     }
 }
